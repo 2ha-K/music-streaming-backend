8c88cb373f8ac577d2800879e58788f5456fc425
Sharkiko <145565841+Sharkiko@users.noreply.github.com>
2026-04-30 23:58:56 -0700

    chore: set up track skeleton
    - Definitions for all track functions have been created, but not implemented

--- services/track_service.py
+++ services/track_service.py
@@ -3,3 +3,14 @@
 - search_tracks()
 - get_track_by_id()
 """
+from db import get_connection
+from utils.db_utils import rows_to_dict, row_to_dict
+
+def get_all_tracks():
+    pass
+
+def search_tracks(title=None, artist=None, album=None):
+    pass
+
+def get_track_by_id(trackKey):
+    pass
