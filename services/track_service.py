c4331012fe9b0117ec85d1985e872ef88e396a5b
Sharkiko <145565841+Sharkiko@users.noreply.github.com>
2026-05-01 00:10:24 -0700

    feat: get all tracks

--- services/track_service.py
+++ services/track_service.py
@@ -7,7 +7,16 @@ from db import get_connection
 from utils.db_utils import rows_to_dict, row_to_dict
 
 def get_all_tracks():
-    pass
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("select * from track;")
+    tracks = cur.fetchall();
+
+    cur.close();
+    conn.close();
+
+    return rows_to_dict(tracks);
 
 def search_tracks(title=None, artist=None, album=None):
     pass
