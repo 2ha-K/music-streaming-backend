360fbb7b69d8965c2cf6a035cf1605c253b0bb9e
Sharkiko <145565841+Sharkiko@users.noreply.github.com>
2026-05-01 00:59:39 -0700

    refactor: track root returns track no offset
    - get_tracks_start_api() refers to get_tracks_offset_api with a value of 0 as its the same code

--- routers/track_router.py
+++ routers/track_router.py
@@ -7,8 +7,12 @@ router = APIRouter(
 )
 
 @router.get("/")
-def get_tracks_start():
-    result = get_tracks_offset()
+def get_tracks_start_api():
+    return get_tracks_offset(0)
+
+@router.get("/{track_offset}")
+def get_tracks_offset_api(track_offset: int):
+    result = get_tracks_offset(offset=track_offset)
     if not result:
         return {"state": "No tracks found", "tracks": []}
     return {"state": "Tracks found", "tracks": result}
