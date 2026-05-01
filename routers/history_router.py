6bdebb366b8bb317e30a64e9c894504076b3f3b6
Sharkiko <145565841+Sharkiko@users.noreply.github.com>
2026-05-01 14:40:31 -0700

    feat: log history

--- routers/history_router.py
+++ routers/history_router.py
@@ -13,3 +13,8 @@ def get_history_api(userkey: int = -1):
     if not history:
         return {"state": "No history"}
     return {"history": history}
+
+@router.post("/log")
+def get_history_api(userkey: int = -1, trackkey: int = -1):
+    result = log_history(user_key=userkey, track_key=trackkey)
+    return {"result": result}
--- services/history_service.py
+++ services/history_service.py
@@ -4,6 +4,7 @@
 """
 
 from db import get_connection
+import psycopg2
 from utils.db_utils import rows_to_dict
 
 def get_history(user_key=-1):
@@ -22,5 +23,26 @@ def get_history(user_key=-1):
 
     return rows_to_dict(history)
 
-def log_history():
-    pass
+def log_history(user_key=-1, track_key=-1):
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            "INSERT INTO listeninghistory (i_userkey, i_trackkey) VALUES (%s, %s)",
+            (user_key, track_key)
+        )
+
+        conn.commit()
+        return {"message": "Track added to history"}
+    except psycopg2.errors.NotNullViolation:
+        conn.rollback()
+        return {"error": "Missing required field"}
+    except psycopg2.Error as e:
+        conn.rollback()
+        return {"error": str(e)}
+    except Exception as e:
+        conn.rollback()
+        return {"error": str(e)}
+    finally:
+        cur.close()
+        conn.close()
